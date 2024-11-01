8d684702bad523c8bdbbc00e443a03cccb693ebd
arpan-sarkar <arpansarkar69@outlook.com>
2024-11-01 18:50:44 +0530

    sa

--- item2/item2.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const menuIcon = document.querySelector(".menu-icon");
-    const navList = document.querySelector(".nav-list");
-
-    menuIcon.addEventListener("click", () => {
-        navList.classList.toggle("active");
-    });
-});
