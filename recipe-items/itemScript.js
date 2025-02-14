e636f92fd91316c32ceb1189926cec9a00f3c29c
arpan-sarkar <arpansarkar69@outlook.com>
2025-02-14 23:01:21 +0530

    update some thing

--- recipe-items/itemScript.js
+++ recipe-items/itemScript.js
@@ -1,8 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
     const menuIcon = document.querySelector(".menu-icon");
-    const navList = document.querySelector(".nav-list");
+    const navList = document.querySelector(".nav-bar");
 
     menuIcon.addEventListener("click", () => {
         navList.classList.toggle("active");
     });
 });
+
+let footerText = document.querySelector("footer")
+footerText.textContent = "© 2025 all right reserved"
