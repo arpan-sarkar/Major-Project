7be6ae89e562c52b8fe81cd23b365a0ce50324c0
arpan-sarkar <arpansarkar69@outlook.com>
2025-04-30 12:26:04 +0530

    fix all recipe item

--- recipe-items/itemScript.js
+++ recipe-items/itemScript.js
@@ -1,12 +1,18 @@
-const menuIcon = document.querySelector('#menu-icon');
-const navBar = document.querySelector('.nav-bar');
+// const menuIcon = document.querySelector('#menu-icon');
+// const navBar = document.querySelector('.nav-bar');
 
-menuIcon.addEventListener('click', () => {
-    navBar.classList.toggle('show-nav');
-    menuIcon.classList.toggle('active');
+// menuIcon.addEventListener('click', () => {
+//     navBar.classList.toggle('show-nav');
+//     menuIcon.classList.toggle('active');
+// });
+
+const menuIcon = document.getElementById("menu-icon");
+const navBar = document.getElementById("nav-bar");
+
+menuIcon.addEventListener("click", () => {
+  menuIcon.classList.toggle("active");
+  navBar.classList.toggle("active");
 });
-
-
 
 
 const scrollToTopBtn = document.getElementById("scrollToTopBtn");
