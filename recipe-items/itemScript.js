e5caab8a97b1fc5bbd98364bccc37f4c3a5bca2a
arpan-sarkar <arpansarkar69@outlook.com>
2025-04-30 12:20:17 +0530

    fix all recipe item

--- recipe-items/itemScript.js
+++ recipe-items/itemScript.js
@@ -1,11 +1,27 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const menuIcon = document.querySelector(".menu-icon");
-    const navList = document.querySelector(".nav-bar");
+const menuIcon = document.querySelector('#menu-icon');
+const navBar = document.querySelector('.nav-bar');
 
-    menuIcon.addEventListener("click", () => {
-        navList.classList.toggle("active");
-    });
+menuIcon.addEventListener('click', () => {
+    navBar.classList.toggle('show-nav');
+    menuIcon.classList.toggle('active');
 });
 
-let footerText = document.querySelector("footer")
-footerText.textContent = "© 2025 all right reserved"
+
+
+
+const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 200) {
+        scrollToTopBtn.style.display = "block";
+    } else {
+        scrollToTopBtn.style.display = "none";
+    }
+});
+
+scrollToTopBtn.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
